a846b1d2c5bb4f838744a98a8c38c47fab7db884
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-05-28 12:03:31 -0400

    init

---
+++ src/consts.ts
@@ -0,0 +1,49 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Flavia's Sweets";
+export const SITE_DESCRIPTION = "Designing delicious cakes for all occasions.";
+
+export const NAV_ITEMS = [
+	{ text: "About Us", href: "/#about" },
+	{ text: "Gallery", href: "/#gallery" },
+	{ text: "FAQ", href: "/#faq" },
+	{ text: "Contact", href: "/#contact" },
+];
+
+export const FAQ_ITEMS = [
+	{
+		question: "What types of sweets do you offer?",
+		answer:
+			"We offer a wide variety of sweets including custom cakes, cupcakes, cookies, and more. Whether you're looking for something classic or a unique creation, we have something to satisfy every sweet tooth.",
+	},
+	{
+		question: "How far in advance should I place my order?",
+		answer:
+			"We recommend placing your order at least two weeks in advance to ensure availability. For larger or more complex orders, a month’s notice is preferred.",
+	},
+	{
+		question: "Do you deliver?",
+		answer:
+			"Yes, we offer delivery within South Florida. Delivery fees vary based on location and the size of your order. You can also opt for pickup at our location.",
+	},
+	{
+		question: "What is your cancellation policy?",
+		answer:
+			"Cancellations must be made at least one week before the scheduled delivery or pickup date to receive a full refund. For cancellations made less than one week in advance, a partial refund may be available.",
+	},
+	{
+		question: "How do I store my cakes and sweets?",
+		answer:
+			"For best results, keep cakes refrigerated and bring them to room temperature before serving. Cookies and other sweets should be stored in an airtight container at room temperature.",
+	},
+	{
+		question: "What payment methods do you accept?",
+		answer: "We accept PayPal, Venmo and cash payments.",
+	},
+	{
+		question: "How can I contact you for more information?",
+		answer:
+			"You can reach us via email, instagram, or through the contact form on our website. We’re here to answer any questions you may have and help you with your order.",
+	},
+];
